419bf439b994d90862bb876a482d59f0db2bc444
JBernardoRebelo <bernasfattaker@gmail.com>
2019-11-30 14:56:37 +0000

    Read file test

--- Program.cs
+++ Program.cs
@@ -6,8 +6,26 @@ namespace IMDB_DATABASE
 {
     public class Program
     {
+        // Ao nível de uma classe
+        const string appName = "MyIMDBSearcher";
+        private const string fileTitleBasics = "title.basics.tsv.gz";
+        private const string fileTitleRatings = "title.ratings.tsv.gz";
+
         private static void Main(string[] args)
         {
+            // Ao nível de um método
+            // Caminho completo da pasta contendo os ficheiros de dados
+            string folderWithFiles = Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                appName);
+
+            // Caminho completo de cada um dos ficheiros de dados
+            string fileTitleBasicsFull = Path.Combine(folderWithFiles, fileTitleBasics);
+            string fileTitleRatingsFull = Path.Combine(folderWithFiles, fileTitleRatings);
+
+
+            TitleLoader.OutputTestFile(fileTitleBasicsFull);
+
             // Call loop
 
             // Debug **************************************************
