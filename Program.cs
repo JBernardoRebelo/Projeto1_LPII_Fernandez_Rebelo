41d76fe1a036a14425f9661f9472275e94827e7d
JBernardoRebelo <bernasfattaker@gmail.com>
2019-11-30 00:01:18 +0000

    Add starter classes with properties. Start LoadTitles Logic

--- Program.cs
+++ Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 
 namespace IMDB_DATABASE
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Call loop
+
+            // Debug **************************************************
+            string line;
+            string[] splitLine;
+
+            line = "ttas";
+
+            string firstChars = line[0].ToString() + line[1].ToString();
+            Console.WriteLine(firstChars);
+
+            // Split lines in tabs
+            splitLine = line.Split('\t').ToArray();
+
+            if (firstChars == "tt")
+            {
+
+            }
         }
     }
 }
---
+++ TitleLoader.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace IMDB_DATABASE
+{
+    /// <summary>
+    /// Class to instantiate titles and store them in Collections
+    /// </summary>
+    public class TitleLoader
+    {
+        public IEnumerable<ITitle> LoadTitlesBasic(string filename)
+        {
+            // Title basic params
+            string id = default;
+            string type = default;
+            string primTitle = default;
+            string origiTitle = default;
+            bool isAdult = default;
+            int startYear = default;
+            int endYear = default;
+            int runTime = default;
+            HashSet<string> genres = default;
+
+            // Line
+            string line;
+            string[] splitLine;
+            string firstCharsOfLine;
+
+            // List of titlesBasic
+            ICollection<TitleBasic> titlesBasic = new List<TitleBasic>();
+
+            // Instantiate Stream reader
+            StreamReader file = new StreamReader(filename);
+
+            // Sort file
+            while ((line = file.ReadLine()) != null)
+            {
+                firstCharsOfLine = line[0].ToString() + line[1].ToString();
+                Console.WriteLine(firstCharsOfLine);
+
+                // Split lines in tabs
+                splitLine = line.Split('\t').ToArray();
+
+                if (firstCharsOfLine == "tt")
+                {
+                    // Split line to corresponding title properties
+                    // Etc
+                }
+            }
+
+            // Close the file
+            file.Close();
+
+            // Returns the list
+            return titlesBasic;
+        }
+    }
+}
