efe0c2c4e86b51122d160c1907a30a2cd742621f
Miguel Fernandez <miguelrf95@gmail.com>
2019-12-03 17:53:05 +0000

    Create new class for file handling, remove file handling methods from Program.cs

--- Program.cs
+++ Program.cs
@@ -1,16 +1,7 @@
-﻿using System;
-using System.IO;
-using System.IO.Compression;
-
-namespace IMDB_DATABASE
+﻿namespace IMDB_DATABASE
 {
     public class Program
     {
-        // Class consts
-        const string appName = "MyIMDBSearcher";
-        private const string fileTitleBasics = "title.basics.tsv.gz";
-        private const string fileTitleRatings = "title.ratings.tsv.gz";
-
         private static void Main(string[] args)
         {
             // **********************************************************
@@ -24,54 +15,11 @@ namespace IMDB_DATABASE
 
             // Call loop, accepts the readable files - so far only basics
             SearchLoop loop = new SearchLoop
-                (MakeReadableBasics(), MakeReadableRatings());
+                (FileHandler.MakeReadableBasics(),
+                 FileHandler.MakeReadableRatings());
 
             loop.ActualLoop();
 
         }
-
-
-        // Returns a streamReader file to use - title basics
-        private static StreamReader MakeReadableBasics()
-        {
-            string folderWithFiles
-                = Path.Combine(Environment.GetFolderPath
-                (Environment.SpecialFolder.LocalApplicationData),
-                appName);
-
-            // Caminho completo de cada um dos ficheiros de dados
-            string fileTitleBasicsFull = Path.Combine(folderWithFiles,
-                fileTitleBasics);
-
-            FileStream fs = new FileStream(fileTitleBasicsFull,
-                FileMode.Open, FileAccess.Read);
-            GZipStream zipFile = new GZipStream(fs,
-                CompressionMode.Decompress);
-            StreamReader file = new StreamReader(zipFile);
-
-            return file;
-        }
-
-        // Returns a streamReader file to use - title ratings
-        private static StreamReader MakeReadableRatings()
-        {
-            // Access needed folder
-            string folderWithFiles
-                = Path.Combine(Environment.GetFolderPath
-                (Environment.SpecialFolder.LocalApplicationData),
-                appName);
-
-            string fileTitleRatingsFull = Path.Combine(folderWithFiles,
-                fileTitleRatings);
-
-            FileStream fs = new FileStream(fileTitleRatingsFull,
-                FileMode.Open, FileAccess.Read);
-            GZipStream zipFile = new GZipStream(fs,
-                CompressionMode.Decompress);
-            StreamReader file = new StreamReader(zipFile);
-
-            return file;
-        }
-
     }
 }
