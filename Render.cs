85c85a41ed9f691ca4b34f0a726f030fc1d7c728
Miguel Fernandez <miguelrf95@gmail.com>
2019-12-06 15:59:41 +0000

    Add method to show title list with ratings (INCOMPLETE). Add method to show RAM usage

--- Render.cs
+++ Render.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+
 
 namespace IMDB_DATABASE
 {
@@ -10,20 +12,20 @@ namespace IMDB_DATABASE
         /// <summary>
         /// Accepts a title basic and prints all info about it
         /// </summary>
-        public Action<TitleBasic> PrintTitleInfo = tb
+        public Action<TitleBasic> PrintSearchInfo = tb
             => Console.WriteLine($"Type: {tb.Type}, " +
                 $"Primary Title: {tb.PrimTitle}, " +
                 $"For Adults: {tb.IsAdult}, Start Year: {tb.StartYear}," +
                 $"End Year: {tb.EndYear} Genres: {tb.Genres}");
 
         /// <summary>
-        /// Method to introduce the user to the program and explain functions
+        /// Method to introduce the user to the program and explain functions  
         /// </summary>
         public void Greetings()
         {
             // REVIEW USER MESSAGES
             Console.WriteLine("Welcome to a console-based IMDB database. \n" +
-                              "Please select the type of search you want to do: \n");
+                        "Please select the type of search you want to do: \n");
             Console.WriteLine("To search for a title press t; \n" +
                               "To search for a actor/actress press a; \n" +
                               "To quit the program press q.");
@@ -37,6 +39,14 @@ namespace IMDB_DATABASE
             Console.Clear();
             Console.WriteLine("You are now searching for titles. \n" +
                 "Please input the title you want to search for:");
+        }
+
+        private void ShowTitles(TitleBasic tb, TitleRating tr)
+        {
+            Console.WriteLine($"Type: {tb.Type}, " +
+                $"Primary Title: {tb.PrimTitle}, " +
+                $"For Adults: {tb.IsAdult}, Start Year: {tb.StartYear}," +
+                $"End Year: {tb.EndYear} Genres: {tb.Genres}");
         }
 
         /// <summary>
@@ -58,5 +68,13 @@ namespace IMDB_DATABASE
             Console.WriteLine("Thank you for using the program!");
         }
 
+
+        // DEBUG METHOD ******************************************************
+        public void ShowRAMUsage()
+        {
+            Console.WriteLine("My app is occupying "
+            + (Process.GetCurrentProcess().VirtualMemorySize64 / 1024 / 1024)
+            + " megabytes of memory");
+        }
     }
 }
