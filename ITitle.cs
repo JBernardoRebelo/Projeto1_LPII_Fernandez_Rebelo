8a17e3faba5ad84f9d58058b7034e755112c39d8
Miguel Fernandez <miguelrf95@gmail.com>
2019-12-06 19:21:09 +0000

    Implement IComparer<T> and IComparable<T>

--- ITitle.cs
+++ ITitle.cs
@@ -1,10 +1,13 @@
-﻿namespace IMDB_DATABASE
+﻿using System;
+using System.Collections.Generic;
+
+namespace IMDB_DATABASE
 {
     /// <summary>
     /// Interface to be extended in diferent
     /// Types of Titles (basic, rating, etc)
     /// </summary>
-    public interface ITitle
+    public interface ITitle : IComparer<ITitle>, IComparable<ITitle>
     {
         /// <summary>
         /// ID - tconst
