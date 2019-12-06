a7b256f13da2d99198d2421b409abfd7cbf60d90
Miguel Fernandez <miguelrf95@gmail.com>
2019-12-06 15:51:22 +0000

    Delete commented methods. Change SearchLoop instance variable name.

--- Program.cs
+++ Program.cs
@@ -4,22 +4,12 @@
     {
         private static void Main(string[] args)
         {
-            // **********************************************************
-            // Static method call LoadTitlesBasic - static for debug
-            //Console.WriteLine("\nBASIC FILE\n");
-            //TitleLoader.LoadTitlesBasic(MakeReadableBasics());
-            //Console.WriteLine("\nRATING'S FILE\n");
-            //TitleLoader.LoadTitlesRating(MakeReadableRatings());
-            // **********************************************************
-
-
             // Call loop, accepts the readable files - so far only basics
-            SearchLoop loop = new SearchLoop
+            SearchLoop searchLoop = new SearchLoop
                 (FileHandler.MakeReadableBasics(),
                  FileHandler.MakeReadableRatings());
 
-            loop.ActualLoop();
-
+            searchLoop.Loop();
         }
     }
 }
