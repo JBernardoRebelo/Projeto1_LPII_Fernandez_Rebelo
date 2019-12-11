22e93480ad2a5b25dac15882eb66fe8ec86cbcd3
Miguel Fernandez <miguelrf95@gmail.com>
2019-12-11 15:42:21 +0000

    Add methods for filter options

--- SearchLoop.cs
+++ SearchLoop.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace IMDB_DATABASE
 {
@@ -11,24 +12,42 @@ namespace IMDB_DATABASE
     {
         // Class variables
         /// <summary>
-        /// Variable to store user input
+        /// Variable to store user input.
         /// </summary>
         private string _uInput;
 
         /// <summary>
+        /// Variable to store title that is being search for
+        /// </summary>
+        private string _searchedTitle;
+
+        /// <summary>
+        /// Variable to be used as a index indentifier within a search
+        /// </summary>
+        private int _searchIndex;
+
+        private bool _endSearch;
+
+        /// <summary>
         /// Instance of the Render class where all output messages exist
         /// </summary>
-        private Render _render;
+        private readonly Render _render;
 
         /// <summary>
         /// Collection that contains basic title info
         /// </summary>
-        private ICollection<ITitle> _titlesBasic;
+        private readonly ICollection<ITitle> _titlesBasic;
 
         /// <summary>
         /// Collection that contains title ratings
         /// </summary>
-        private ICollection<ITitle> _titlesRating;
+        private readonly ICollection<ITitle> _titlesRating;
+
+        /// <summary>
+        /// Collection that contains all information from
+        /// other title collections
+        /// </summary>
+        private readonly ICollection<ITitle> _titles;
 
         /// <summary>
         /// Constructor to initiate the search loop
@@ -43,97 +62,56 @@ namespace IMDB_DATABASE
         {
             _render = new Render();
             _uInput = default;
+            _endSearch = false;
+            _searchIndex = default;
+            _searchedTitle = default;
 
             // Load titles to collection
             _titlesBasic = TitleLoader.LoadTitlesBasic(fileBasic);
             _titlesRating = TitleLoader.LoadTitlesRating(fileRating);
+
+            //// Check if there are duplicates
+            //_titles = _titlesBasic.GroupBy(
+            //    i => i.ID).Select(t => t.First()).ToList();
+
+            //// Check if 
+            //foreach (ITitle rating in _titlesRating)
+            //{
+            //    if (_titles.Where(x => x.ID == rating.ID).Count() == 0)
+            //    {
+            //        _titles.Add(rating);
+            //    }
+            //}
         }
 
-        // Menu tipo de pesquisa
-
-        // Menu de pesquisa por titulos
-
-        // Receber keyword
-
-        // Apresentar resultados (20 por "pagina")
-
-        // Menu de pesquisa por pessoas
-
-        // Receber keyword
-
-        // Apresentar resultados
-
-        // Menu de saida
-
-        // Menu de acesso a titulos
-
-        // Receber opção
-
-        // Opção de aceder a info detalhada do titulo
-
-        // Apresentar detalhes do titulo
-
-        // Voltar ao titulo "pai" (no caso de uma serie)
-
-        // Voltar à pesquisa de titulos
-
-        // Voltar ao menu anterior
-
-        // Opção de saida do programa
-
         /// <summary>
-        /// Main method where the search loop occours
+        /// Main method where the search loop begins
         /// </summary>
-        public void Loop()
+        public void BeginSearch()
         {
             while (true)
             {
                 _render.Greetings();
+
                 // DEBUG METHOD **********************************************
                 _render.ShowRAMUsage();
                 // END DEBUG *************************************************
 
-                // Debug******************************************************
-                int i = 0;
-                foreach (ITitle t in _titlesRating)
-                {
-                    i++;
-                    if (t is TitleRating)
-                    {
-                    OutputTestFile((TitleRating)t);
-                    }
-                    if (i > 30)
-                    {
-                        break;
-                    }
-                }
-                // ***
-
-                GetUserInput();
+                UserSearchOption();
                 TypeOfSearch();
             }
         }
 
-        // Debug method for output ********************************************
-        private static void OutputTestFile(TitleRating t)
-        {
-            Console.Write($"{t.ID} - ");
-            Console.Write($"{t.AvgRating} - ");
-            Console.Write($"{t.NumVotes} - \n");
-        }
-        // End of debug method ************************************************
-
         /// <summary>
-        /// Method where the type of search in the database is selected
+        /// Method where the type of search to be done in the database
+        /// is chosen
         /// </summary>
         private void TypeOfSearch()
         {
             switch (_uInput)
             {
-                case "a":
-                    break;
-
                 case "t":
+                    _endSearch = false;
                     _uInput = null;
                     _render.SearchForTitles();
                     TitleSearch();
@@ -147,35 +125,48 @@ namespace IMDB_DATABASE
         }
 
         /// <summary>
-        /// Method to get user input to be used in menus.
+        /// Method to get user search option input to be used in the main menu.
+        /// If user input is invalid, calls Render's ErroMessage() and calls
+        /// Render's Greetings() again.
         /// </summary>
-        private void GetUserInput()
+        private void UserSearchOption()
         {
             do
             {
                 _uInput = Console.ReadLine().ToLower();
 
-                if (_uInput == null || _uInput != "t" && _uInput != "q")
+                if (_uInput != "t" && _uInput != "q")
                 {
                     _render.ErrorMessage();
+                    _render.Greetings();
                 }
 
-            } while (_uInput == null && _uInput != "t" && _uInput != "q");
+            } while (_uInput != "t" && _uInput != "q");
         }
 
         /// <summary>
-        /// Method to search user selected title to search for.
+        /// Method where user inputs title name to be searched for.
+        /// After input, it is shown the results. Calls ShowSearchTitle()
         /// </summary>
         private void TitleSearch()
         {
             string titleToSearch = GetTitle();
+
             _uInput = titleToSearch;
 
-            ShowSearchTitle(_uInput);
+            if (_uInput == "b")
+            {
+                BeginSearch();
+            }
+            else
+            {
+                ShowTitlesGeneral(_uInput);
+            }
         }
 
         /// <summary>
-        /// Method to get the title to be searched for.
+        /// Method to get the title to be searched for. If input is invalid,
+        /// calls Render's ErrorMessage() and calls Render's SearchForTitles().
         /// </summary>
         /// <returns> User title choice. </returns>
         private string GetTitle()
@@ -186,9 +177,151 @@ namespace IMDB_DATABASE
             {
                 titleToSearch = Console.ReadLine();
 
-            } while (titleToSearch == null);
+                if (titleToSearch == null || titleToSearch == "")
+                {
+                    _render.ErrorMessage();
+                    _render.SearchForTitles();
+                }
+
+            } while (titleToSearch == null || titleToSearch == "");
+
+            _searchedTitle = titleToSearch;
 
             return titleToSearch;
+        }
+
+
+        /// <summary>
+        /// Method that prints title that contain user input string
+        /// </summary>
+        /// <param name="name">
+        /// User input title string
+        /// </param>
+        private void ShowTitlesGeneral(string name)
+        {
+            // This must pause every 20 iterations
+            _searchIndex = 0;
+
+            // Query result colection
+            IEnumerable<TitleBasic> results =
+                from title in _titlesBasic.OfType<TitleBasic>()
+                where title.PrimTitle.Contains(name)
+                select title;
+
+            while (_searchIndex < results.Count() && !_endSearch)
+            {
+                _render.GeneralSearchGUI();
+
+                for (byte i = 0; i < 20 && i < results.Count(); i++)
+                {
+                    ++_searchIndex;
+                    _render.ShowTitles(results.ElementAt(_searchIndex));
+                }
+
+                FilterOptions();
+            }
+
+            //foreach (TitleBasic tb in results)
+            //{
+            //    _render.ShowTitles(tb);
+
+            //    //++j;
+
+            //    if (j >= 20)
+            //    {
+            //        // Wait for user input
+            //        FilterOptions();
+            //        Console.Clear();
+            //        j = 0;
+            //        _render.GeneralSearchGUI();
+            //    }
+        }
+
+        /// <summary>
+        /// Method to show organizing options when showing titles
+        /// </summary>
+        private void FilterOptions()
+        {
+            _render.ShowFilterOptions();
+
+            _uInput = GetUserFilterOptions();
+
+            switch (_uInput)
+            {
+                case "detail":
+                    _uInput = null;
+                    _render.AskForTitleToDetail();
+                    ShowTitleDetails(GetTitleForDetails());
+                    break;
+
+                case "date":
+                    break;
+
+                case "type":
+                    break;
+
+                case "genre":
+                    _render.AskForGenreToFilterBy();
+
+                    break;
+
+                case "back":
+                    _uInput = null;
+                    _searchedTitle = null;
+                    _endSearch = true;
+                    break;
+
+                default:
+                    Console.Clear();
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Method to get user filter option of choice, 
+        /// or none and keep showing other titles
+        /// </summary>
+        /// <returns> String with selected user choice. </returns>
+        private string GetUserFilterOptions()
+        {
+            string filterOption;
+
+            do
+            {
+                filterOption = Console.ReadLine().ToLower();
+
+            } while (filterOption != "" && filterOption != "detail" &&
+                     filterOption != "date" && filterOption != "type" &&
+                     filterOption != "genre" && filterOption != "back");
+
+            return filterOption;
+        }
+
+        /// <summary>
+        /// Method to get user title of choice to get 
+        /// detailed information about.
+        /// </summary>
+        /// <returns> String with chosen title. </returns>
+        private string GetTitleForDetails()
+        {
+            string chosenTitle;
+
+            do
+            {
+                chosenTitle = Console.ReadLine().ToLower();
+
+            } while (chosenTitle == null);
+
+            return chosenTitle;
+        }
+
+        /// <summary>
+        /// Method where user chosen title details are shown.
+        /// </summary>
+        /// <param name="title"> Specific title to be detailed. </param>
+        private void ShowTitleDetails(string title)
+        {
+            // FAZER AMANHA (11/12), PRIMEIRO DESPACHAR MENUS E ORGANIZAÇÃO
         }
 
         /// <summary>
@@ -197,36 +330,6 @@ namespace IMDB_DATABASE
         private void QuitProgram()
         {
             Environment.Exit(0);
-        }
-
-        /// <summary>
-        /// Method that prints title that contain user input string
-        /// </summary>
-        /// <param name="name">
-        /// User input title string
-        /// </param>
-        private void ShowSearchTitle(string name)
-        {
-            // This must pause every 20 iterations
-            int i = 0;
-
-            foreach (TitleBasic tb in _titlesBasic)
-            {
-                if (tb.PrimTitle.Contains(name) ||
-                    tb.OrigiTitle.Contains(name))
-                {
-                    _render.PrintSearchInfo(tb);
-                    ++i;
-                }
-
-                if (i >= 20)
-                {
-                    // Wait for user input
-                    Console.ReadLine();
-                    i = 0;
-                    Console.Clear();
-                }
-            }
         }
     }
 }
